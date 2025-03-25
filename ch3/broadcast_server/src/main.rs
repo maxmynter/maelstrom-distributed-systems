18ba49d85e4920d2a35b3f5357f782203affc967
Max Mynter <maxmynter@me.com>
2025-03-24 23:49:26 -0400

    Implement Broadcast and Read

--- ch3/broadcast_server/src/main.rs
+++ ch3/broadcast_server/src/main.rs
@@ -1,19 +1,21 @@
 use serde::de::Error;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io;
 use std::io::Write;
 use std::sync::{Arc, Mutex};
 
 type NodeId = String;
 type MsgId = u64;
+type NodeMessage = i64;
 
 #[derive(Debug)]
 struct Node {
     node_id: NodeId,
     node_ids: Vec<NodeId>,
     topology: Option<HashMap<NodeId, Vec<NodeId>>>,
+    messages: Arc<Mutex<HashSet<NodeMessage>>>,
     next_message_id: MsgId,
     stdout: Arc<Mutex<std::io::Stdout>>,
     stderr: Arc<Mutex<std::io::Stderr>>,
@@ -25,17 +27,48 @@ impl Node {
         self.next_message_id
     }
 
-    fn log(&mut self, message: &str) -> Result<()> {
+    fn add_message(
+        &mut self,
+        message: NodeMessage,
+    ) -> std::result::Result<(), Box<dyn std::error::Error>> {
+        let was_inserted = {
+            let mut messages = self
+                .messages
+                .lock()
+                .map_err(|e| format!("Failed to acquire lock on messages: {}", e))?;
+            messages.insert(message)
+        };
+        let _ = self.log(&format!(
+            "Node({}): {} message '{}'",
+            self.node_id,
+            if was_inserted {
+                "Inserted"
+            } else {
+                "Already had"
+            },
+            &message
+        ));
+        Ok(())
+    }
+
+    fn read_messages(
+        &mut self,
+    ) -> std::result::Result<Vec<NodeMessage>, Box<dyn std::error::Error>> {
+        let messages = self
+            .messages
+            .lock()
+            .map_err(|e| format!("Failed to aqcuire lock on messages: {}", e))?;
+        let messages_vec = messages.iter().cloned().collect();
+        Ok(messages_vec)
+    }
+
+    fn log(&mut self, text: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
         match self.stderr.lock() {
             Ok(mut err_out_guard) => {
-                let _ = writeln!(err_out_guard, "{}", message)
-                    .map_err(|e| serde_json::Error::custom(e.to_string()));
+                let _ = writeln!(err_out_guard, "{}", text)?;
                 Ok(())
             }
-            Err(e) => Err(serde_json::Error::custom(format!(
-                "Failed to acquire lock on logging: {}",
-                e,
-            ))),
+            Err(e) => Err(format!("Failed to acquire lock on logging: {}", e).into()),
         }
     }
 
@@ -88,6 +121,18 @@ enum MessageBody {
     },
     #[serde(rename = "topology_ok")]
     TopologyOk { msg_id: MsgId, in_reply_to: MsgId },
+    #[serde(rename = "broadcast")]
+    Broadcast { msg_id: MsgId, message: NodeMessage },
+    #[serde(rename = "broadcast_ok")]
+    BroadcastOk { msg_id: MsgId, in_reply_to: MsgId },
+    #[serde(rename = "read")]
+    Read { msg_id: MsgId },
+    #[serde(rename = "read_ok")]
+    ReadOk {
+        msg_id: MsgId,
+        in_reply_to: MsgId,
+        messages: Vec<NodeMessage>,
+    },
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -118,6 +163,7 @@ fn main() -> Result<()> {
             let mut node = Node {
                 node_id: node_id.to_string(),
                 node_ids: node_ids.clone(),
+                messages: Arc::new(Mutex::new(HashSet::new())),
                 topology: None,
                 next_message_id: 0,
                 stdout: Arc::new(Mutex::new(io::stdout())),
@@ -153,9 +199,34 @@ fn main() -> Result<()> {
                         };
                         let _ = node.send(&message.src, response_body);
                     }
+                    MessageBody::Broadcast {
+                        msg_id,
+                        message: broadcast_message,
+                    } => {
+                        let _ = node.add_message(broadcast_message);
+                        let response_body = MessageBody::BroadcastOk {
+                            msg_id: node.get_next_msg_id(),
+                            in_reply_to: msg_id,
+                        };
+                        let _ = node.send(&message.src, response_body);
+                    }
+                    MessageBody::Read { msg_id } => {
+                        let Ok(messages) = node.read_messages() else {
+                            return Err(serde_json::Error::custom(&format!(
+                                "Failed to read messages on node {}",
+                                node.node_id
+                            ))
+                            .into());
+                        };
+                        let response_body = MessageBody::ReadOk {
+                            msg_id: node.get_next_msg_id(),
+                            in_reply_to: msg_id,
+                            messages,
+                        };
+                        let _ = node.send(&message.src, response_body);
+                    }
                     _ => {
                         let _ = node.log("Received message with no known handler");
-                        continue;
                     }
                 }
             }
