5dc90362017cde53ea7f4422af827e497678293e
Max Mynter <maxmynter@me.com>
2025-03-26 11:58:15 -0400

    Factor message handlers out of main fn

--- ch3/broadcast_server/src/main.rs
+++ ch3/broadcast_server/src/main.rs
@@ -1,7 +1,8 @@
-use serde::de::Error;
+use serde::de::Error as SerdeError;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::collections::{HashMap, HashSet};
+use std::error::Error as StdError;
 use std::io;
 use std::io::Write;
 use std::sync::{Arc, Mutex};
@@ -27,10 +28,7 @@ impl Node {
         self.next_message_id
     }
 
-    fn add_message(
-        &mut self,
-        message: NodeMessage,
-    ) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    fn add_message(&mut self, message: NodeMessage) -> std::result::Result<(), Box<dyn StdError>> {
         let was_inserted = {
             let mut messages = self
                 .messages
@@ -51,9 +49,7 @@ impl Node {
         Ok(())
     }
 
-    fn read_messages(
-        &mut self,
-    ) -> std::result::Result<Vec<NodeMessage>, Box<dyn std::error::Error>> {
+    fn read_messages(&mut self) -> std::result::Result<Vec<NodeMessage>, Box<dyn StdError>> {
         let messages = self
             .messages
             .lock()
@@ -65,7 +61,7 @@ impl Node {
     fn messages_contain(
         &mut self,
         message: &NodeMessage,
-    ) -> std::result::Result<bool, Box<dyn std::error::Error>> {
+    ) -> std::result::Result<bool, Box<dyn StdError>> {
         let messages = self
             .messages
             .lock()
@@ -73,7 +69,7 @@ impl Node {
         Ok(messages.contains(&message))
     }
 
-    fn log(&mut self, text: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    fn log(&mut self, text: &str) -> std::result::Result<(), Box<dyn StdError>> {
         match self.stderr.lock() {
             Ok(mut err_out_guard) => {
                 let _ = writeln!(err_out_guard, "{}", text)?;
@@ -103,6 +99,106 @@ impl Node {
         };
         let _ = self.log(&format!("Sent: {}", jsonified));
         Ok(())
+    }
+
+    fn handle_echo(&mut self, message: &Message) -> std::result::Result<(), Box<dyn StdError>> {
+        match &message.body {
+            MessageBody::Echo { msg_id, echo } => {
+                let response_body = MessageBody::EchoOk {
+                    echo: echo.to_string(),
+                    in_reply_to: *msg_id,
+                };
+                let _ = self.send(&message.src, response_body);
+                Ok(())
+            }
+            _ => Err("handle_echo called on different message".into()),
+        }
+    }
+
+    fn handle_topology(&mut self, message: &Message) -> std::result::Result<(), Box<dyn StdError>> {
+        match &message.body {
+            MessageBody::Topology { msg_id, topology } => {
+                self.topology = Some(topology.clone());
+                let response_body = MessageBody::TopologyOk {
+                    in_reply_to: *msg_id,
+                };
+                let _ = self.send(&message.src, response_body);
+                Ok(())
+            }
+            _ => Err("handle_topology called on different message".into()),
+        }
+    }
+
+    fn handle_broadcast(
+        &mut self,
+        message: &Message,
+    ) -> std::result::Result<(), Box<dyn StdError>> {
+        match message.body {
+            MessageBody::Broadcast {
+                msg_id,
+                message: broadcast_message,
+            } => {
+                match self.messages_contain(&broadcast_message) {
+                    Ok(true) => {}
+                    Ok(false) => {
+                        let _ = self.add_message(broadcast_message);
+
+                        // Gossip message to neighbors
+                        if let Some(topology) = &mut self.topology {
+                            let neighbors = match topology.get(&self.node_id) {
+                                Some(neighbors) => neighbors.clone(),
+                                None => Vec::new(),
+                            };
+                            for tgt_node_id in neighbors {
+                                if tgt_node_id == message.src {
+                                    // Skip the origin of the broadcast
+                                    continue;
+                                }
+                                let response_body = MessageBody::Broadcast {
+                                    msg_id: self.get_next_msg_id(),
+                                    message: broadcast_message.clone(),
+                                };
+                                let _ = self.send(&tgt_node_id, response_body);
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        return Err(format!(
+                            "Error checking if node message contains broadcast message: {}",
+                            e
+                        )
+                        .into());
+                    }
+                }
+                // Acknowledge Broadcast
+                let response_body = MessageBody::BroadcastOk {
+                    in_reply_to: msg_id,
+                };
+                let _ = self.send(&message.src, response_body);
+                Ok(())
+            }
+            _ => Err("handle_broadcast called on different message".into()),
+        }
+    }
+    fn handle_read(&mut self, message: &Message) -> std::result::Result<(), Box<dyn StdError>> {
+        match &message.body {
+            MessageBody::Read { msg_id } => {
+                let Ok(messages) = self.read_messages() else {
+                    return Err(serde_json::Error::custom(&format!(
+                        "Failed to read messages on node {}",
+                        self.node_id
+                    ))
+                    .into());
+                };
+                let response_body = MessageBody::ReadOk {
+                    in_reply_to: *msg_id,
+                    messages,
+                };
+                let _ = self.send(&message.src, response_body);
+                Ok(())
+            }
+            _ => Err("handle_read called on different message".into()),
+        }
     }
 }
 
@@ -148,7 +244,7 @@ struct Message {
     body: MessageBody,
 }
 
-fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+fn main() -> std::result::Result<(), Box<dyn StdError>> {
     // Read the node config
     let mut node_wrapper: Option<Node> = None;
 
@@ -185,82 +281,29 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
             continue;
         };
         match &mut node_wrapper {
-            Some(node) => {
-                match message.body {
-                    MessageBody::Echo { msg_id, echo } => {
-                        // Create and stdout the echo response
-                        let response_body = MessageBody::EchoOk {
-                            echo,
-                            in_reply_to: msg_id,
-                        };
-                        let _ = node.send(&message.src, response_body);
-                    }
-                    MessageBody::Topology { msg_id, topology } => {
-                        node.topology = Some(topology);
-                        let response_body = MessageBody::TopologyOk {
-                            in_reply_to: msg_id,
-                        };
-                        let _ = node.send(&message.src, response_body);
-                    }
-                    MessageBody::Broadcast {
-                        msg_id,
-                        message: broadcast_message,
-                    } => {
-                        match node.messages_contain(&broadcast_message) {
-                            Ok(true) => {
-                                continue;
-                            }
-                            Ok(false) => {
-                                let _ = node.add_message(broadcast_message);
-
-                                // Gossip message to neighbors
-                                if let Some(topology) = &mut node.topology {
-                                    let neighbors = match topology.get(&node.node_id) {
-                                        Some(neighbors) => neighbors.clone(),
-                                        None => Vec::new(),
-                                    };
-                                    for tgt_node_id in neighbors {
-                                        let response_body = MessageBody::Broadcast {
-                                            msg_id: node.get_next_msg_id(),
-                                            message: broadcast_message.clone(),
-                                        };
-                                        let _ = node.send(&tgt_node_id, response_body);
-                                    }
-                                }
-                            }
-                            Err(e) => {
-                                return Err(format!(
-                                    "Error checking if node message contains broadcast message: {}",
-                                    e
-                                )
-                                .into());
-                            }
-                        }
-                        // Acknowledge Broadcast
-                        let response_body = MessageBody::BroadcastOk {
-                            in_reply_to: msg_id,
-                        };
-                        let _ = node.send(&message.src, response_body);
-                    }
-                    MessageBody::Read { msg_id } => {
-                        let Ok(messages) = node.read_messages() else {
-                            return Err(serde_json::Error::custom(&format!(
-                                "Failed to read messages on node {}",
-                                node.node_id
-                            ))
-                            .into());
-                        };
-                        let response_body = MessageBody::ReadOk {
-                            in_reply_to: msg_id,
-                            messages,
-                        };
-                        let _ = node.send(&message.src, response_body);
-                    }
-                    _ => {
-                        let _ = node.log("Received message with no known handler");
-                    }
+            Some(node) => match message.body {
+                MessageBody::Echo { msg_id: _, echo: _ } => {
+                    let _ = node.handle_echo(&message);
                 }
-            }
+                MessageBody::Topology {
+                    msg_id: _,
+                    topology: _,
+                } => {
+                    let _ = node.handle_topology(&message);
+                }
+                MessageBody::Broadcast {
+                    msg_id: _,
+                    message: _,
+                } => {
+                    let _ = node.handle_broadcast(&message);
+                }
+                MessageBody::Read { msg_id: _ } => {
+                    let _ = node.handle_read(&message);
+                }
+                _ => {
+                    let _ = node.log("Received message with no known handler");
+                }
+            },
             None => {
                 return Err(format!("Received non-init message before node initialization",).into())
             }
